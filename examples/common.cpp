482d96d8fa16ccc488140dd82554b0e6f310128a
Evgeny Safronov <division494@gmail.com>
2014-11-01 14:58:55 +0300

    [Examples] Examples changing.

--- examples/common.cpp
+++ examples/common.cpp
@@ -2,25 +2,22 @@
 
 #include <epicmeter/benchmark.hpp>
 
+#include <unistd.h>
+
 using namespace epicmeter;
+
+//BENCHMARK(DatetimeGeneration, strftime, native, samples = 30; count = 100000; description = "Very long description") {
+//}
 
 //!=============================================================================
 //! Common usage.
-BENCHMARK(DatetimeGeneration, strftime) {
+BENCHMARK(DatetimeFormatting, strftime) {
     std::time_t time = std::time(0);
     std::tm tm;
     localtime_r(&time, &tm);
     char buffer[64];
     strftime(buffer, 64, "%Y-%m-%d %H:%M:%S", &tm);
     compiler::do_not_optimize(buffer);
-}
-
-//!=============================================================================
-//! Common usage with string test case name.
-BENCHMARK(Increment, Using: i++) {
-    int i = 0;
-    i++;
-    compiler::do_not_optimize(i);
 }
 
 //!=============================================================================
@@ -44,74 +41,91 @@ BENCHMARK(VectorRelative, push_back 10k elements reserved) {
 //!=============================================================================
 //! Sometimes you want to perform iterations yourself. In that case the library
 //! provides you iteration count parameter.
-BENCHMARK(VectorAccepted, insert_front, n) {
-    std::vector<int> v;
+BENCHMARK(DatetimeFormatting, strftime: N provided, n) {
+    std::time_t time = std::time(0);
+    std::tm tm;
+    localtime_r(&time, &tm);
+    char buffer[64];
     for (iteration_type::value_type i = 0; i < n.v; ++i) {
-        v.insert(v.begin(), i);
+        strftime(buffer, 64, "%Y-%m-%d %H:%M:%S", &tm);
+        compiler::do_not_optimize(buffer);
     }
 }
 
 //!=============================================================================
 //! Sometimes you want to perform iterations yourself and you know iteration
 //! count. In that case you should return iteration count from function.
-BENCHMARK_RETURN(VectorReturned, insert_front) {
-    std::vector<int> v;
+BENCHMARK_RETURN(DatetimeFormatting, strftime: N own) {
     iteration_type iters(10000);
+
+    std::time_t time = std::time(0);
+    std::tm tm;
+    localtime_r(&time, &tm);
+    char buffer[64];
     for (iteration_type::value_type i = 0; i < iters.v; ++i) {
-        v.insert(v.begin(), i);
+        strftime(buffer, 64, "%Y-%m-%d %H:%M:%S", &tm);
+        compiler::do_not_optimize(buffer);
     }
     return iters;
 }
 
 //!=============================================================================
 //! Benchmark with bound parameters.
-void insert_front(std::size_t n) {
-    std::vector<int> v;
-    v.reserve(n);
-    for (int i = 0; i < 10000; ++i) {
-        v.insert(v.begin(), i);
-    }
+void format_by_pattern(const char* format) {
+    std::time_t time = std::time(0);
+    std::tm tm;
+    localtime_r(&time, &tm);
+    char buffer[64];
+    strftime(buffer, 64, format, &tm);
+    compiler::do_not_optimize(buffer);
 }
-BENCHMARK_BOUND(VectorParam, insert_front, 100);
-BENCHMARK_BOUND(VectorParam, insert_front, 1000);
-BENCHMARK_BOUND(VectorParam, insert_front, 10000);
+BENCHMARK_BOUND(DatetimeFormatting, format_by_pattern, "%Y-%m-%d");
+BENCHMARK_BOUND(DatetimeFormatting, format_by_pattern, "%H:%M:%S");
+BENCHMARK_BOUND(DatetimeFormatting, format_by_pattern, "%Y-%m-%d %H:%M:%S");
 
 //! Benchmark with multiple bound parameters.
-void insert_front_multi(std::size_t n, double pi) {
-    std::vector<int> v;
-    v.reserve(n);
-    for (int i = 0; i < 10000; ++i) {
-        v.insert(v.begin(), i);
-    }
+void format_by_pattern_multiple(const char* format, double pi) {
+    std::time_t time = std::time(0);
+    std::tm tm;
+    localtime_r(&time, &tm);
+    char buffer[64];
+    strftime(buffer, 64, format, &tm);
+    compiler::do_not_optimize(buffer);
 }
-BENCHMARK_BOUND(VectorParam, insert_front_multi, 100, 3.1415);
-BENCHMARK_BOUND(VectorParam, insert_front_multi, 1000, 2 * 3.1415);
-BENCHMARK_BOUND(VectorParam, insert_front_multi, 10000, 3 * 3.1415);
+BENCHMARK_BOUND(DatetimeFormatting, format_by_pattern_multiple, "%Y-%m-%d", 3.1415);
+BENCHMARK_BOUND(DatetimeFormatting, format_by_pattern_multiple, "%H:%M:%S", 2 * 3.1415);
+BENCHMARK_BOUND(DatetimeFormatting, format_by_pattern_multiple, "%Y-%m-%d %H:%M:%S", 3 * 3.1415);
 
 //!=============================================================================
 //! Benchmark with bound parameters and iteration count provided.
-void insert_front_provided(iteration_type iters, std::size_t n) {
-    std::vector<int> v;
-    v.reserve(n);
-    for (int i = 0; i < iters.v; ++i) {
-        v.insert(v.begin(), i);
+void format_by_pattern_provided(iteration_type iters, const char* format) {
+    std::time_t time = std::time(0);
+    std::tm tm;
+    localtime_r(&time, &tm);
+    char buffer[64];
+    for (iteration_type::value_type i = 0; i < iters.v; ++i) {
+        strftime(buffer, 64, format, &tm);
+        compiler::do_not_optimize(buffer);
     }
 }
-BENCHMARK_BOUND(VectorParam, insert_front_provided, 100);
-BENCHMARK_BOUND(VectorParam, insert_front_provided, 1000);
-BENCHMARK_BOUND(VectorParam, insert_front_provided, 10000);
+BENCHMARK_BOUND(DatetimeFormatting, format_by_pattern_provided, "%Y-%m-%d");
+BENCHMARK_BOUND(DatetimeFormatting, format_by_pattern_provided, "%H:%M:%S");
+BENCHMARK_BOUND(DatetimeFormatting, format_by_pattern_provided, "%Y-%m-%d %H:%M:%S");
 
 //!=============================================================================
 //! Benchmark with bound parameters and iteration count returned.
-iteration_type insert_front_returned(std::size_t n) {
+iteration_type format_by_pattern_returned(const char* format) {
     iteration_type iters(10000);
-    std::vector<int> v;
-    v.reserve(n);
-    for (int i = 0; i < iters.v; ++i) {
-        v.insert(v.begin(), i);
+    std::time_t time = std::time(0);
+    std::tm tm;
+    localtime_r(&time, &tm);
+    char buffer[64];
+    for (iteration_type::value_type i = 0; i < iters.v; ++i) {
+        strftime(buffer, 64, format, &tm);
+        compiler::do_not_optimize(buffer);
     }
     return iters;
 }
-BENCHMARK_BOUND(VectorParam, insert_front_returned, 100);
-BENCHMARK_BOUND(VectorParam, insert_front_returned, 1000);
-BENCHMARK_BOUND(VectorParam, insert_front_returned, 10000);
+BENCHMARK_BOUND(DatetimeFormatting, format_by_pattern_returned, "%Y-%m-%d");
+BENCHMARK_BOUND(DatetimeFormatting, format_by_pattern_returned, "%H:%M:%S");
+BENCHMARK_BOUND(DatetimeFormatting, format_by_pattern_returned, "%Y-%m-%d %H:%M:%S");
